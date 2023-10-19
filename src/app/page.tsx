2834e625133ff632c65866e32f1934f1ff11882a
Sasank-C <chunduri.s@northeastern.edu>
2023-10-19 00:05:18 -0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,34 +22,34 @@ function useHandlerState<T>(
   return [value, ref, updateValue];
 }
 
-export const metadata: Metadata = {
-  openGraph: {
-    title: process.env.NEXT_PUBLIC_META_TITLE ?? MINSTA_META.title,
-    description:
-      process.env.NEXT_PUBLIC_META_DESCRIPTION ?? MINSTA_META.description,
-    images: [
-      {
-        type: "image/png",
-        url: process.env.NEXT_PUBLIC_META_IMAGE ?? MINSTA_META.image,
-        width: "1200",
-        height: "630",
-      },
-    ],
-  },
+// export const metadata: Metadata = {
+//   openGraph: {
+//     title: process.env.NEXT_PUBLIC_META_TITLE ?? MINSTA_META.title,
+//     description:
+//       process.env.NEXT_PUBLIC_META_DESCRIPTION ?? MINSTA_META.description,
+//     images: [
+//       {
+//         type: "image/png",
+//         url: process.env.NEXT_PUBLIC_META_IMAGE ?? MINSTA_META.image,
+//         width: "1200",
+//         height: "630",
+//       },
+//     ],
+//   },
 
-  twitter: {
-    card: "summary_large_image",
-    title: process.env.NEXT_PUBLIC_META_TITLE ?? MINSTA_META.title,
-    description:
-      process.env.NEXT_PUBLIC_META_DESCRIPTION ?? MINSTA_META.description,
-    siteId: "1467726470533754880",
-    creator: "Mintbase",
-    images: process.env.NEXT_PUBLIC_META_IMAGE ?? MINSTA_META.image,
-  },
-  title: process.env.NEXT_PUBLIC_META_TITLE ?? MINSTA_META.title,
-  description:
-    process.env.NEXT_PUBLIC_META_DESCRIPTION ?? MINSTA_META.description,
-};
+//   twitter: {
+//     card: "summary_large_image",
+//     title: process.env.NEXT_PUBLIC_META_TITLE ?? MINSTA_META.title,
+//     description:
+//       process.env.NEXT_PUBLIC_META_DESCRIPTION ?? MINSTA_META.description,
+//     siteId: "1467726470533754880",
+//     creator: "Mintbase",
+//     images: process.env.NEXT_PUBLIC_META_IMAGE ?? MINSTA_META.image,
+//   },
+//   title: process.env.NEXT_PUBLIC_META_TITLE ?? MINSTA_META.title,
+//   description:
+//     process.env.NEXT_PUBLIC_META_DESCRIPTION ?? MINSTA_META.description,
+// };
 
 export default function Home() {
   const [lock, lockRef, setLock] = useHandlerState(true);
